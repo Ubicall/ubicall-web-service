eea879578e9018cd8883ef7ddcab0666a55efe68
waleed samy <waleed_samy@ymail.com>
2015-09-10 17:32:20 +0300

    gruntfile not depend on settings.js

--- settings.js
+++ settings.js
@@ -111,8 +111,6 @@ module.exports = {
   plistHost: config.defaultPlistHost,
   widgetHost: config.endPoints.widgetDeploy,
 
-  //where to deploy API documentation
-  apiDeployFolder: "/var/www/html/docs/",
   call: {
     status: {
       progress: 'PROGRESS',
