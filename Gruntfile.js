fcb6450512aa12ed972291f8717fde01b3bfe7c1
waleed samy <waleed_samy@ymail.com>
2015-10-01 13:51:57 +0200

    Add grunt task to deploy nginx configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,9 @@
 module.exports = function(grunt) {
 
+  var appConfig = {
+    nginx: 'conf/nginx'
+  };
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jsdoc: {
@@ -13,18 +17,32 @@ module.exports = function(grunt) {
     },
     copy: {
       docs: {
-        expand : true,
-        cwd : './docs/',
-        src : ['**/*.*'],
+        expand: true,
+        cwd: './docs/',
+        src: ['**/*.*'],
         dest: "/var/www/html/docs/"
-      }
+      },
+      nginx: {
+        expand: true,
+        cwd: appConfig.nginx,
+        src:  ['**/*.conf'],
+        dest: '/etc/nginx/conf.d/'
+      },
     }
   });
 
   grunt.loadNpmTasks('grunt-jsdoc');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-nginx');
 
+  grunt.registerTask('preserve', 'connect resources for development mode deployment', [
+    'jsdoc:docs', 'copy:docs', 'copy:nginx', 'nginx:restart'
+  ]);
 
-  grunt.registerTask('default', ['jsdoc:docs', 'copy:docs']);
+  grunt.registerTask('prebuild', 'connect resources for production mode deployment', [
+    'jsdoc:docs', 'copy:docs', 'copy:nginx', 'nginx:restart'
+  ]);
+
+  grunt.registerTask('default', ['jsdoc:docs', 'copy:docs', 'copy:nginx', 'nginx:restart']);
 
 }
