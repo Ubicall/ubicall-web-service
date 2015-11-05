303bd3a8e1b94f91c58e723e36ee2da9a03baca7
rawan hamdi <rawan.hamdi@sandcti.com>
2015-11-05 11:32:30 +0200

    fixing function name

--- api/v1/3rd/zendesk/ticket.js
+++ api/v1/3rd/zendesk/ticket.js
@@ -11,9 +11,9 @@
  var ServerError = require("././utils/errors").ServerError;
 
 /**
-  Create a zendesk ticket
+	Create a zendesk ticket
 * @param {String} req.body.subject - Subject of the ticket
-* @param {String} req.body.body -Body  of the ticket
+* @param {String} req.body.body -Body	of the ticket
 * @param {String} req.body.priority -Priority of the ticket {urgent, high, normal, or low}
 * @param {String} req.body.type -Type of the ticket {problem, incident, question, or task}
 * @return HTTP status 200 if ticket created successfully
@@ -22,29 +22,31 @@
 * @memberof API
 */
 
- function createTicket(req,res,next){
-    var username = "founders@ubicall.com";
-     var api_token = "ZeFnzD7Dhu9hYt5TlUya8WCnaozbQF6MJLozokGj";
-     var domain ="ubicall";
-     var client = zendesk.createClient({
-         username:  username,
-         token:api_token,
-         remoteUri: "https://"+ domain +".zendesk.com/api/v2"
-       });
-console.log(req.body);
-       var ticket = {
-                      "ticket":req.body
-                    };
+function createTicket(req, res, next) {
+		var username = req.user.zendesk.email;
+		var api_token = req.user.zendesk.token;
+		var domain = req.user.zendesk.domain;
+		var client = zendesk.createClient({
+				username: username,
+				token: api_token,
+				remoteUri: "https://" + domain + ".zendesk.com/api/v2"
+		});
 
-        client.tickets.create(ticket,  function(err, req, result) {
-          if (err){   return next(new ServerError(err, req.path));}
-          console.log(JSON.stringify(result, null, 2, true));
-          res.status(200).json(result);
-        });
- }
+		var ticket = {
+				"ticket": req.body
+		};
+
+		client.tickets.create(ticket, function(err, req, result) {
+				if (err) {
+						res.send(err);
+				}
+				console.log(JSON.stringify(result, null, 2, true));
+				res.status(200).json(result);
+		});
+}
 
 
 
- module.exports = {
-   createTicket:createTicket
- };
+module.exports = {
+		createTicket: createTicket
+};
