46429e192ab906a50ed7dfee432c71e456151813
waleed samy <waleed_samy@ymail.com>
2015-11-17 11:48:53 +0200

    rewrite this code as a developer

--- api/v1/3rd/zendesk/ticket.js
+++ api/v1/3rd/zendesk/ticket.js
@@ -6,15 +6,44 @@
  * @namespace ticket
  */
 
+var when = require("when");
 var request = require("request");
-var zendesk = require("node-zendesk");
 var ServerError = require("../../utils/errors").ServerError;
 var log = require("../../../../log");
-var when = require("when");
-
 
 /**
- *	Create a zendesk ticket
+ * create zendesk ticket with request.js based on [gist](https://gist.github.com/waleedsamy/d074b459591de5155af8)
+ */
+function postZendeskTicket(zdcred, ticket) {
+    return when.promise(function(resolve, reject) {
+        if (Object.keys(ticket).length) {
+            return reject("ticket contain nothing!");
+        }
+        var options = {
+            url: zdcred.main + "/tickets.json",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            auth: {
+                user: zdcred.username,
+                pass: zdcred.token
+            },
+            json: true,
+            body: ticket
+        };
+        request(options, function(error, response, body) {
+            if (error) {
+                return reject(error || response.statusCode);
+            } else {
+                return resolve(body);
+            }
+        });
+    });
+}
+
+/**
+ * Create a zendesk ticket
  * @param {Object} req.body - request body object, contain valid [ticket fields](https://developer.zendesk.com/rest_api/docs/core/ticket_fields)
  * @return HTTP status 200 if ticket created successfully
  * @throws {@link ServerError} if failed to create a ticket
@@ -24,46 +53,29 @@ var when = require("when");
  * @memberof API
  */
 
-function createTicket(req, res) {
-    return when.promise(function(resolve, reject) {
-      var fields=req.body;    var custom_fields=[];
-      for (var key in fields) {
-        if(/^\d+$/.test(key)){
-        custom_fields.push({"id": parseInt(key),"value":fields[key]});
-      }
-      }
-        var options = {
-           url: req.user.zendesk.main+"/tickets.json",
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            auth: {
-               user: req.user.zendesk.username,
-               pass: req.user.zendesk.token
-            },
-            json: true,
-            body: {
-                "ticket": {
-                    "subject": req.body.Subject,
-                    "description": req.body.Description,
-                    "priority": req.body.Priority,
-                    "status": req.body.Status,
-                    "type": req.body.Type,
-                    "group":req.body.Group,
-                    "assignee":req.body.Assignee,
-                    "custom_fields": custom_fields
-                }
-            }
-        };
-        request(options, function(error, response, body) {
-            if (error) {
-              return reject(error ||  response.statusCode);
-            } else {
-              return resolve(response.data);
-            }
+function createTicket(req, res, next) {
+    var ticket = {
+        custom_fields: []
+    };
+
+    for (var key in req.body) {
+        if (/^\d+$/.test(key)) {
+            ticket.custom_fields.push({
+                "id": parseInt(key, 10),
+                "value": req.body[key]
+            });
+        } else {
+            ticket[key] = req.body[key];
+        }
+    }
+
+    postZendeskTicket(req.user.zendesk, ticket).then(function(done) {
+        res.status(200).json({
+            message: "zendesk ticket submitted successfully"
         });
-});
+    }).otherwise(function(err) {
+        return next(new ServerError(err, req.path, "error creating zendesk ticket"));
+    });
 }
 
 module.exports = {
